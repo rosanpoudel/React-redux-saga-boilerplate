97eae15f96979a8c75aa09a96aadb0661669a6e7
rosanpoudel <roshan.poudel@grepsr.com>
2022-06-12 17:54:49 +0545

    fixes

--- src/Routes/privateRoute.js
+++ src/Routes/privateRoute.js
@@ -3,13 +3,13 @@ import { Route, Redirect } from "react-router-dom";
 import LocalDb from "../localStorage";
 
 export default function PrivateRoute({ path, exact, component: Component }) {
-	return (
-		<Route
-			path={path}
-			exact={exact}
-			render={(props) =>
-				LocalDb.getSessions() ? <Component /> : <Redirect to="/login" />
-			}
-		/>
-	);
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(props) =>
+        LocalDb.getSessions() ? <Component /> : <Redirect to="/" />
+      }
+    />
+  );
 }
